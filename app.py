f4372213698c41e1bcec2e6a32ba952c9b50741e
Kwok cheong <wongkwokcheong@hotmail.com>
2020-05-12 12:24:24 +0800

    Added UIUX for events and search bar

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pymongo
 from datetime import datetime
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
+import re
 
 # load env file
 load_dotenv()
@@ -18,13 +19,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def helloworld():
+def home():
     return render_template("index.template.html")
 
 @app.route('/show_programmes', methods=["GET"])
 def show_programmes(): 
-    all_programmes = client[DB_NAME].programmes.find()
-    return render_template('show_programmes.template.html', results = all_programmes)
+    search_name = request.args.get('search-by')
+
+    search_criteria = {}
+    if search_name is not "": 
+        search_criteria["title"] = re.compile(r'{}'.format(search_name), re.I)
+
+    projection={'title'}
+    cursor = client[DB_NAME].programmes.find(search_criteria, projection)
+    
+    return render_template('show_programmes.template.html', results = cursor)
 
 @app.route('/create_programmes', methods=["GET","POST"])
 def create_programmes():
@@ -35,8 +44,13 @@ def create_programmes():
         client[DB_NAME].programmes.insert_one({
             "title": request.form.get('programme_title'),
             "duration": request.form.get('programme_duration'),
-            "distance": request.form.get('programme_distance'),
-            "description": request.form.get("programme_description")
+            "difficulty": request.form.get('programme_difficulty'),
+            "tag": request.form.get("programme_tags"),
+            "category": request.form.get('programme_category'),
+            "description": request.form.get("programme_description"),
+            "date": datetime.strptime(request.form.get('programme_date'), "%Y-%m-%d"),
+            "time": datetime.strptime(request.form.get("programme_time"), '%H:%M'),
+            "location": request.form.get("programme_location")
         })
     return redirect(url_for("show_programmes"))
 
@@ -64,11 +78,16 @@ def edit_programme(programme_id):
             "$set": {
                 "title": request.form.get('programme_title'),
                 "duration": request.form.get('programme_duration'),
-                "distance": request.form.get('programme_distance'),
                 "description": request.form.get("programme_description")
             }
         })
         return redirect(url_for("show_programmes"))
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(
