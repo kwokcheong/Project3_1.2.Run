673b24fcb812d82ab3e64bbefde0ee0a22f63072
Kwok cheong <wongkwokcheong@hotmail.com>
2020-05-20 23:39:56 +0800

    Editted script.js added semicolon

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -6,21 +6,19 @@ function validateForm() {
         let description = document.forms["form"]["programme_description"].value;
         
         if (title === ""){
-            alert("name must be filled")
+            alert("name must be filled");
             return false;
         }
         if (date === ""){
-            alert("date must be filled")
+            alert("date must be filled");
             return false;
         }
         if (time === ""){
-            alert("time must be filled")
+            alert("time must be filled");
             return false;
         }
         if (description === ""){
-            alert("Description must be filled")
+            alert("Description must be filled");
             return false;
         }
-
-
 }
